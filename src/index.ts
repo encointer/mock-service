ed98fb91a8ad26d057f31af58d0de205e9b3f1fb
pifragile <pigu@bluewin.ch>
2022-10-18 12:55:19 +0800

    switch from rpc-websockts to ws

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,40 @@
-import { Server as WebSocketServer } from "rpc-websockets";
 import { ApiPromise, WsProvider } from "@polkadot/api";
 import { encointer_rpc_endpoint } from "./consts";
-import { register_rpc_methods } from "./rpc";
+import { handleMessage } from "./rpc";
 import { putCommunityObject } from "./db";
 import { CommunityObject } from "./types";
+import { getStorage } from "./storage";
+import { WebSocketServer, WebSocket, RawData } from "ws";
 
 async function main() {
-    var server = new WebSocketServer({
-        port: 8080,
-        host: "localhost",
-    });
     const wsProvider = new WsProvider(encointer_rpc_endpoint);
     const api = await ApiPromise.create({ provider: wsProvider });
 
-    register_rpc_methods(server, api);
+    let test = api.createType(
+        "Vec<EncointerPrimitivesCommunitiesCommunityIdentifier>",
+        [
+            {
+                geohash: "sqm1v",
+                digest: "0xf08c911c",
+            },
+        ]
+    );
+    console.log(test.toHex());
+
+    let test2 = api.createType(
+        "Vec<EncointerPrimitivesCommunitiesCommunityIdentifier>",
+        "0x0473716d3176f08c911c"
+    );
+    console.log(test2.toHuman());
+
+    const wss = new WebSocketServer({ port: 8080 });
+
+    wss.on("connection", function connection(ws) {
+        ws.on("message", async function message(data) {
+            //console.log(data.toString());
+            await handleMessage(api, ws, data);
+        });
+    });
 
     const communityObject: CommunityObject = {
         currentPhase: "attesting",
--- src/rpc.ts
+++ src/rpc.ts
@@ -1,40 +1,49 @@
-import { Server as WebSocketServer } from "rpc-websockets";
-import { ApiPromise } from "@polkadot/api";
 import { advancePhase } from "./lib/runtime";
 import { getCommunityObject, putCommunityObject } from "./db";
+import { ApiPromise } from "@polkadot/api";
+import { encointer_rpc_endpoint } from "./consts";
+import { getStorage } from "./storage";
+import { WebSocket, RawData } from "ws";
 
-async function getResultOrErr<T>(promise: Promise<T>) {
-    try {
-        return await promise;
-    } catch (e) {
-        let message = "Unknown Error";
-        if (e instanceof Error) message = e.message;
-        return { error: message };
+function relay(ws: WebSocket, data: RawData) {
+    const encointer_rpc = new WebSocket(encointer_rpc_endpoint);
+    encointer_rpc.on("open", function open() {
+        encointer_rpc.send(data);
+    });
+    encointer_rpc.on("message", function message(data) {
+        ws.send(data.toString());
+    });
+}
+export async function handleMessage(
+    api: ApiPromise,
+    ws: WebSocket,
+    data: RawData
+) {
+    let request = JSON.parse(data.toString());
+    let method = request.method;
+    switch (method) {
+        case "encointer_getReputations":
+            break;
+        case "encointer_getLocations":
+            break;
+        case "state_getStorage":
+            getStorage(api, request.params[0]);
+            break;
+        case "advancePhase":
+            let cid = request.params[0];
+            let communityObject = await getCommunityObject(cid);
+            communityObject = advancePhase(communityObject);
+            await putCommunityObject(cid, communityObject);
+            ws.send(communityObject.currentPhase);
+            break;
+        case "chain_getBlockHash":
+        case "state_getRuntimeVersion":
+        case "system_chain":
+        case "system_properties":
+        case "rpc_methods":
+        case "state_getMetadata":
+        case "system_health":
+        case "system_properties":
+            relay(ws, data);
     }
-}
-
-async function processExtrinsic(api: ApiPromise, encodedExtrinsic: string) {
-    let ex = await api.tx(encodedExtrinsic).toHuman();
-}
-
-export function register_rpc_methods(server: WebSocketServer, api: ApiPromise) {
-    server.register("sum", function (params) {
-        return params[0] + params[1];
-    });
-
-    server.register("author_submitAndWatchExtrinsic", async function (params) {
-        return await processExtrinsic(api, params[0]);
-    });
-
-    server.register("advancePhase", async function (params) {
-        return getResultOrErr(
-            (async () => {
-                let cid = params[0];
-                let communityObject = await getCommunityObject(cid);
-                communityObject = advancePhase(communityObject);
-                await putCommunityObject(cid, communityObject);
-                return communityObject.currentPhase;
-            })()
-        );
-    });
 }
