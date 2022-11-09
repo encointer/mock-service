9479c7be9eff2ec4defe09538308ca55149a3d22
pifragile <pigu@bluewin.ch>
2022-11-09 09:26:43 +0100

    remove comments

--- src/rpc.ts
+++ src/rpc.ts
@@ -30,83 +30,22 @@ export async function handleMessage(
     switch (method) {
         case "encointer_getLocations":
             let cid = request.params[0];
-            // {"jsonrpc":"2.0","result":[{"lat":"47.3860442609734562325","lon":"8.5167555883526802063"},{"lat":"47.3863389067466229676","lon":"8.5233766213059425354"},{"lat":"47.3869987892637922755","lon":"8.51989746093749822364"},{"lat":"47.3885922795468204072","lon":"8.51957626640796661377"},{"lat":"47.3891715535333091225","lon":"8.5129961371421813965"},{"lat":"47.3895923857361651699","lon":"8.5164836794137954712"},{"lat":"47.3896700146103739826","lon":"8.52360427379608154297"},{"lat":"47.39043449402448260344","lon":"8.51080611348152160645"},{"lat":"47.3922230783101596785","lon":"8.51907670497894109474"},{"lat":"47.39408059955918872674","lon":"8.5130444169044494629"},{"lat":"47.38784547884539222196","lon":"8.52683097124099731445"}],"id":92}
             break;
         case "encointer_getAllBalances":
             let accountId = request.params[0]
-            // {"jsonrpc":"2.0","result":[[{"geohash":"0x7530716a39","digest":"0x77f79df7"},{"principal":"17.9322839837634529508","lastUpdate":1449018}]],"id":11}
             break;
         case "encointer_getReputations":
             accountId = request.params[0]
-            // "result": [
-            //     [
-            //         2,
-            //         {
-            //             "communityIdentifier": {
-            //                 "geohash": "0x73716d3176",
-            //                 "digest": "0xf08c911c"
-            //             },
-            //             "reputation": "VerifiedLinked"
-            //         }
-            //     ],
-            //     [
-            //         3,
-            //         {
-            //             "communityIdentifier": {
-            //                 "geohash": "0x73716d3176",
-            //                 "digest": "0xf08c911c"
-            //             },
-            //             "reputation": "VerifiedLinked"
-            //         }
-            //     ],
-            //     [
-            //         1,
-            //         {
-            //             "communityIdentifier": {
-            //                 "geohash": "0x73716d3176",
-            //                 "digest": "0xf08c911c"
-            //             },
-            //             "reputation": "VerifiedLinked"
-            //         }
-            //     ],
-            //     [
-            //         4,
-            //         {
-            //             "communityIdentifier": {
-            //                 "geohash": "0x73716d3176",
-            //                 "digest": "0xf08c911c"
-            //             },
-            //             "reputation": "UnverifiedReputable"
-            //         }
-            //     ]
-            // ],
+
             break;
         case "encointer_getAggregatedAccountData":
             cid = request.params[0];
             accountId = request.params[1];
-            // "result": {
-            //     "global": {
-            //         "ceremonyPhase": "Assigning",
-            //         "ceremonyIndex": 3
-            //     },
-            //     "personal": {
-            //         "participantType": "Bootstrapper",
-            //         "meetupIndex": 1,
-            //         "meetupLocationIndex": 3,
-            //         "meetupTime": null,
-            //         "meetupRegistry": [
-            //             "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty",
-            //             "5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y",
-            //             "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
-            //         ]
-            //     }
-            // },
             break;
         case "encointer_getAllCommunities":
             let allCommunities = await getAllCommunitiyObjects();
             let result = getAllCommunites(allCommunities);
             ws.send(JSON.stringify(result));
-            //{"jsonrpc":"2.0","result":[{"cid":{"geohash":"0x7530716a39","digest":"0x77f79df7"},"name":"Leu Zurich"},{"cid":{"geohash":"0x6b78746b76","digest":"0x45387a38"},"name":"Aslah"}],"id":30}
             break;
         case "author_submitAndWatchExtrinsic":
             let extrinsic = api.tx(request.params[0]);
