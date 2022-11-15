b8a04c46f48b334792989bc480679856e23cd352
pifragile <pigu@bluewin.ch>
2022-11-15 16:33:42 +0100

    optimize cid parsing

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -5,13 +5,18 @@ export function maybeHexToString(s: string) {
     else return s
 }
 
+export function maybeStringToHex(s: string) {
+    if(s.startsWith("0x")) return s
+    else return "0x" + Buffer.from(s, 'utf8').toString('hex');
+}
+
 export function parseCid(cid: string): CommunityIdentifierObject {
     return {
-        geohash: cid.substring(0, 5),
-        digest: cid.substring(5, 9),
+        geohash: cid.substring(0, 12),
+        digest: cid.substring(12, 22),
     };
 }
 
 export function cidToString(cid: CommunityIdentifierObject): string {
-    return maybeHexToString(cid.geohash) + maybeHexToString(cid.digest);
+    return maybeStringToHex(cid.geohash) + maybeStringToHex(cid.digest);
 }
