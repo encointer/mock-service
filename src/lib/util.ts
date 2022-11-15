1ba1a4ed82d15975c2f551691cec3535e57780be
pifragile <pigu@bluewin.ch>
2022-11-15 15:35:17 +0100

    fix cid parsing

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,12 +1,17 @@
 import { CommunityIdentifierObject } from "../types";
 
+export function maybeHexToString(s: string) {
+    if(s.startsWith("0x")) return Buffer.from(s.slice(2), "hex").toString();
+    else return s
+}
+
 export function parseCid(cid: string): CommunityIdentifierObject {
     return {
-        geohash: cid.substring(0, 12),
-        digest: cid.substring(12, 22),
+        geohash: cid.substring(0, 5),
+        digest: cid.substring(5, 9),
     };
 }
 
 export function cidToString(cid: CommunityIdentifierObject): string {
-    return cid.geohash + cid.digest;
+    return maybeHexToString(cid.geohash) + maybeHexToString(cid.digest);
 }
